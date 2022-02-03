3e9725c1a9b04eeab1b9254fc1ec462f43862e5c
Einstein_5 <55026418+Asman5@users.noreply.github.com>
2022-02-03 12:56:44 +0600

    Create a.cpp

---
+++ lesson_1/a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // int - integer, char - 1 symbol, string - few chars, float - 5.65,  
+    int a, b;
+    char c;
+    cin >> a;
+    cin >> b;
+    cout << a + b;
+
+    return 0;
+}
